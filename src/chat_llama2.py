d5ddae7bcd9706b72700855b2a1f156b52a54d04
m_hikichi <hijiki.mshr19980119@outlook.com>
2023-07-24 19:49:37 +0900

    feat: 過去のチャット内容をメッセージに反映させるためのコード修正

--- src/chat_llama2.py
+++ src/chat_llama2.py
@@ -2,17 +2,20 @@ from llama_cpp import Llama
 
 
 model = Llama(model_path="/tmp/llama2-model/llama-2-7b-chat.ggmlv3.q8_0.bin")
+conversation_history = ""
 
 while True:
-    input_text = input("> ")
-    input_text = "user: " + input_text + "com:"
+    user_input = input("> ")
+    conversation_history += "user: " + user_input + "\nllama:"
 
     output = model(
-        input_text,
+        conversation_history,
         max_tokens=1024,
-        stop=["user:", "com:"],
+        stop=["user:", "llama:"],
         echo=True,
     )
 
-    input_text += " "
-    print("> " + output["choices"][0]["text"].replace(input_text, "") + "\n")
+    conversation_history += " "
+    llama_reply = output["choices"][0]["text"].replace(conversation_history, "")
+    print("> " + llama_reply + "\n")
+    conversation_history = output["choices"][0]["text"]
