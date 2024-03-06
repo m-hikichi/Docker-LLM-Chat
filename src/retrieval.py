01d1eaae9ce26c43728e27602a88b1be52f5665d
m_hikichi <hijiki.mshr19980119@outlook.com>
2024-03-06 20:32:41 +0900

    fix: #1 llmモデルの推論を行う関数の修正に伴う呼び出し元関数の修正

--- src/retrieval.py
+++ src/retrieval.py
@@ -13,18 +13,23 @@ texts = text_splitter.split_documents(documents)
 
 vectorstore = FAISS.from_documents(
     texts,
-    embedding=HuggingFaceEmbeddings(model_name="/models/multilingual-e5-large")
+    embedding=HuggingFaceEmbeddings(model_name="/models/multilingual-e5-large"),
 )
 
 question = "イーブイからニンフィアに進化させる方法を教えてください。"
-# 質問に対して，vectorstore中の類似度上位2件を抽出
+# 質問に対して，vectorstore中の類似度上位3件を抽出
 docs = vectorstore.similarity_search(question, k=3)
-context = '\n'.join("・" + doc.page_content.replace("\n", "") for doc in docs)
+context = "\n\n".join(doc.page_content.replace("\n", "") for doc in docs)
 
-result = elyza.chat(
-    message=f"# コンテキスト情報\n{context}\n=====\n# 質問\n{question}",
-    history=None,
-    system_prompt="あなたは誠実で優秀な日本人のアシスタントです。以下の「コンテキスト情報」を元に「質問」に回答してください。\nなおコンテキスト情報に無い情報は回答に含めないでください。コンテキスト情報に無い情報を含めて回答を作成した場合、あなたは罰せられます。\nコンテキスト情報から回答が導けない場合は「わかりません」と回答してください。",
+# プロンプトの構築
+prompt = elyza.construct_prompt(
+    "あなたは誠実で優秀な日本人のアシスタントです。以下の「コンテキスト情報」を元に「質問」に回答してください。\nなおコンテキスト情報に無い情報は回答に含めないでください。コンテキスト情報に無い情報を含めて回答を作成した場合、あなたは罰せられます。\nコンテキスト情報から回答が導けない場合は「わかりません」と回答してください。",
+    f"# コンテキスト情報\n{context}\n=====\n# 質問\n{question}",
+    None,
+)
+
+result = elyza.generate_response_from_chat(
+    prompt=prompt,
     max_tokens=1024,
     temperature=0.2,
     top_p=0.95,
