88766a887319ec1bbe03214869b07b654c4a3fff
m_hikichi <hijiki.mshr19980119@outlook.com>
2024-08-03 12:05:00 +0900

    docs: 型ヒント・docstringの追加

--- src/llm_api.py
+++ src/llm_api.py
@@ -2,6 +2,7 @@ import requests
 from typing import Optional
 from langchain_openai import OpenAI
 from langchain_core.prompts import PromptTemplate
+from langchain_core.pydantic_v1 import Field, SecretStr
 
 
 class LLMAPIException(Exception):
@@ -16,25 +17,33 @@ class LLMAPIException(Exception):
 
 
 def fetch_llm_api_model(
-    api_url: str,
+    api_url: Optional[str] = Field(default=None, alias="base_url"),
+    api_key: Optional[SecretStr] = Field(default=None, alias="api_key"),
+    model: str = Field(default="gpt-3.5-turbo-instruct", alias="model"),
     temperature: float = 0.7,
+    batch_size: int = 20,
     max_tokens: int = 256,
     top_p: float = 1.0,
 ) -> OpenAI:
     """
     Args:
-        api_url :
-        temperature :
-        max_tokens :
-        top_p :
+        api_url : Base URL path for API requests, leave blank if not using a proxy or service emulator.
+        api_key : Automatically inferred from env var `OPENAI_API_KEY` if not provided.
+        model : Model name to use.
+        temperature : What sampling temperature to use.
+        batch_size : Batch size to use when passing multiple documents to generate.
+        max_tokens : The maximum number of tokens to generate in the completion. -1 returns as many tokens as possible given the prompt and the models maximal context size.
+        top_p : Total probability mass of tokens to consider at each step.
     """
     try:
         response = requests.get(api_url + "/models")
         if response.status_code == 200:
             llm_model = OpenAI(
                 openai_api_base=api_url,
-                openai_api_key="dummy_api_key",
+                openai_api_key=api_key,
+                model=model,
                 temperature=temperature,
+                batch_size=batch_size,
                 max_tokens=max_tokens,
                 top_p=top_p,
             )
@@ -50,7 +59,9 @@ def fetch_llm_api_model(
 if __name__ == "__main__":
     # load llm model
     llm_model = fetch_llm_api_model(
-        "http://text-generation-webui:5000/v1",
+        api_url="http://ollama:11434/v1",
+        api_key="dummy_api_key",
+        model="ELYZA:8B-Q4_K_M",
     )
 
     # create prompt
