cd54cee6cce913ef0865ba2dbca96a938e3ecfeb
m_hikichi <hijiki.mshr19980119@outlook.com>
2024-08-11 10:11:36 +0900

    pref: #7 ollama apiを用いたRAGに修正

--- src/retrieval_augmented_generation.py
+++ src/retrieval_augmented_generation.py
@@ -1,30 +1,14 @@
 import langchain_core
 from typing import Iterable, List, Optional
 from pathlib import Path
-from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import FAISS
 from langchain_core.vectorstores import VectorStoreRetriever
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough
-from llamacpp import load_llamacpp_model
-
-
-def load_embedding_model(
-    model_name: str = "sentence-transformers/all-mpnet-base-v2",
-    show_progress: bool = False,
-) -> HuggingFaceEmbeddings:
-    """
-    Initialize the sentence_transformer.
-    Args:
-        model_name : Model name to use.
-        show_progress : Whether to show a progress bar.
-    """
-    return HuggingFaceEmbeddings(
-        model_name=model_name,
-        show_progress=show_progress,
-    )
+from llms.llm_api import fetch_llm_api_model
+from embeddings.embedding_api import fetch_embedding_model
 
 
 def load_documents(
@@ -112,36 +96,41 @@ if __name__ == "__main__":
         chunk_overlap=0,
     )
 
+    embedding_model = fetch_embedding_model(
+        base_url="http://ollama:11434",
+        model_name="multilingual-e5-large",
+    )
+
     semantic_retriever = construct_semantic_retriever(
         documents=splitted_documents,
-        embedding_model=HuggingFaceEmbeddings(
-            model_name="/workspace/models/multilingual-e5-large"
-        ),
+        embedding_model=embedding_model,
         k=3,
     )
 
-    llm_model = load_llamacpp_model(
-        model_path="/workspace/models/ELYZA-japanese-Llama-2-13b-fast-instruct-gguf/ELYZA-japanese-Llama-2-13b-fast-instruct-q5_K_M.gguf",
-        n_ctx=2048,
-        n_gpu_layers=-1,
+    llm_model = fetch_llm_api_model(
+        api_url="http://ollama:11434/v1",
+        api_key="dummy_api_key",
+        model="ELYZA:8B-Q4_K_M",
         temperature=0.2,
         top_p=0.95,
-        top_k=50,
+        max_tokens=2048,
     )
 
     prompt = PromptTemplate(
         input_variables=["context", "question"],
-        template="""<s>[INST] <<SYS>>
+        template="""<|start_header_id|>system<|end_header_id|>
+
 あなたは誠実で優秀な日本人のアシスタントです。以下の「コンテキスト情報」を元に「質問」に回答してください。
 なおコンテキスト情報に無い情報は回答に含めないでください。
 コンテキスト情報から回答が導けない場合は「わかりません」と回答してください。
-<</SYS>>
+<|eot_id|><|start_header_id|>user<|end_header_id|>
 
 # コンテキスト情報
 {context}
 
-# 質問
-{question}[/INST]""",
+{question}<|eot_id|><|start_header_id|>assistant<|end_header_id|>
+
+""",
     )
 
     chain = (
@@ -153,3 +142,4 @@ if __name__ == "__main__":
     question = "イーブイからニンフィアに進化させる方法を教えてください。"
     for text in chain.stream(question):
         print(text, flush=True, end="")
+    print()
