43724910fd4c84d15b089188587caecb16e66e29
m_hikichi <hijiki.mshr19980119@outlook.com>
2024-08-24 09:38:15 +0900

    feat: モデルを変更するたびにテンプレートプロンプトをコード上で書き換える必要をなくすために，ChatPromptを用いたコードに修正

--- src/modules/llms/llm_api.py
+++ src/modules/llms/llm_api.py
@@ -1,8 +1,10 @@
-import requests
+import os
 from typing import Optional
-from langchain_openai import OpenAI
-from langchain_core.prompts import PromptTemplate
-from langchain_core.pydantic_v1 import Field, SecretStr
+
+import requests
+from langchain_core.messages import SystemMessage
+from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
+from langchain_openai import ChatOpenAI
 
 
 class LLMAPIException(Exception):
@@ -17,69 +19,55 @@ class LLMAPIException(Exception):
 
 
 def fetch_llm_api_model(
-    api_url: Optional[str] = Field(default=None, alias="base_url"),
-    api_key: Optional[SecretStr] = Field(default=None, alias="api_key"),
-    model: str = Field(default="gpt-3.5-turbo-instruct", alias="model"),
+    model: str = "gpt-3.5-turbo-instruct",
     temperature: float = 0.7,
-    batch_size: int = 20,
     max_tokens: int = 256,
-    top_p: float = 1.0,
-) -> OpenAI:
+) -> ChatOpenAI:
     """
     Args:
-        api_url : Base URL path for API requests, leave blank if not using a proxy or service emulator.
-        api_key : Automatically inferred from env var `OPENAI_API_KEY` if not provided.
         model : Model name to use.
         temperature : What sampling temperature to use.
-        batch_size : Batch size to use when passing multiple documents to generate.
         max_tokens : The maximum number of tokens to generate in the completion. -1 returns as many tokens as possible given the prompt and the models maximal context size.
-        top_p : Total probability mass of tokens to consider at each step.
     """
     try:
-        response = requests.get(api_url + "/models")
-        if response.status_code == 200:
-            llm_model = OpenAI(
-                openai_api_base=api_url,
-                openai_api_key=api_key,
-                model=model,
-                temperature=temperature,
-                batch_size=batch_size,
-                max_tokens=max_tokens,
-                top_p=top_p,
-            )
+        # Making an API request to fetch models
+        response = requests.get(os.environ["OPENAI_API_BASE"] + "/models")
+        response.raise_for_status()  # Automatically handles non-200 status codes
 
-            return llm_model
-        else:
-            raise LLMAPIException(api_url, response.status_code)
+        # Assuming OpenAI is a class that needs these parameters
+        llm_model = ChatOpenAI(
+            model=model,
+            temperature=temperature,
+            max_tokens=max_tokens,
+        )
 
-    except requests.exceptions.RequestException:
-        raise LLMAPIException(api_url)
+        return llm_model
+
+    except requests.exceptions.RequestException as e:
+        raise LLMAPIException(
+            os.environ["OPENAI_API_BASE"],
+            e.response.status_code if e.response else None,
+        )
 
 
 if __name__ == "__main__":
     # load llm model
     llm_model = fetch_llm_api_model(
-        api_url="http://ollama:11434/v1",
-        api_key="dummy_api_key",
         model="ELYZA:8B-Q4_K_M",
     )
 
     # create prompt
-    prompt = PromptTemplate(
-        input_variables=["query"],
-        template="""<|start_header_id|>system<|end_header_id|>
-
-あなたは誠実で優秀な日本人のアシスタントです。<|eot_id|><|start_header_id|>user<|end_header_id|>
-
-{query}<|eot_id|><|start_header_id|>assistant<|end_header_id|>
-
-""",
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            SystemMessage("あなたは誠実で優秀な日本人のアシスタントです。"),
+            HumanMessagePromptTemplate.from_template("{query}"),
+        ]
     )
 
     # construct chain
     chain = prompt | llm_model
 
     # inference
-    for text in chain.stream({"query": "有名な犬種をリスト形式で教えてください"}):
-        print(text, flush=True, end="")
+    for text in chain.stream({"query": "こんにちは"}):
+        print(text.content, flush=True, end="")
     print()
